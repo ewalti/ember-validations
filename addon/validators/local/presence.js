ffc78472633ff029eccfae65bcc6a292057e260e
Eric Walti <ewalti@gmail.com>
2014-12-05 14:33:56 -0800

    add length check for presence, isBlank is supposed to fire?

--- addon/validators/local/presence.js
+++ addon/validators/local/presence.js
@@ -15,6 +15,8 @@ export default Base.extend({
     if (this.options.message === undefined) {
       this.options.message = Messages.render('blank', this.options);
     }
+    // fire on length change
+    this.model.addObserver(this.property + '.length', this, this._validate);
   },
   call: function() {
     if (Ember.isBlank(get(this.model, this.property))) {
